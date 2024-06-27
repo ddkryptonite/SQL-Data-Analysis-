5788ecedea3aafc412b64d0be21f2198e931e5d4
Daniel <ddkryptonite@gmail.com>
2024-06-27 11:09:16 +0000

    Adding .sql scripts

---
+++ Customer_Segmentation.sql
@@ -0,0 +1,26 @@
+SELECT *
+FROM users
+LIMIT 20;
+
+SELECT email, birthday
+FROM users
+WHERE birthday BETWEEN '1980-01-01' AND '1989-12-31'
+ORDER BY birthday;
+
+SELECT email, created_at
+FROM users
+WHERE created_at < '2017-05-01'
+ORDER BY created_at;
+
+SELECT email
+FROM users
+WHERE test = 'bears';
+
+SELECT email
+FROM users
+WHERE campaign LIKE 'BBB%';
+
+SELECT email
+FROM users
+WHERE campaign LIKE '%-2';
+
